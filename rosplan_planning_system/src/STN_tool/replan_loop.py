ffd313f227d8f440c3e32c2d12a103639cbdb277
Parisa Zehtabi <parisa.zehtabi@kcl.ac.uk>
2019-08-28 20:35:56 +0100

    print the plan and problem file from the replan loop

--- rosplan_planning_system/src/STN_tool/replan_loop.py
+++ rosplan_planning_system/src/STN_tool/replan_loop.py
@@ -38,16 +38,19 @@ while not goal_achieved and replans<10:
     try:
         pg = rospy.ServiceProxy('/rosplan_problem_interface/problem_generation_server', Empty)
         time.sleep(3)
+        #rospy.Subscriber('', String, callback)
         if not pg():
             print('NO PROBLEM')
 
         #back time to 1
         pt = rospy.ServiceProxy('/sim_clock/set_time_scale', SetInt)
         pi = rospy.ServiceProxy('/rosplan_planner_interface/planning_server', Empty)
+        os.system('rostopic echo -n1 /rosplan_problem_interface/problem_instance')
         if not pi():
             replans += 1
         else:
             pp = rospy.ServiceProxy('/rosplan_parsing_interface/parse_plan', Empty)
+            os.system('rostopic echo -n1 /rosplan_planner_interface/planner_output')
             pp()
 
             if use_stn_tool:
